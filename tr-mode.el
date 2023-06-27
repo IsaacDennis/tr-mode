33e8fa940cbf55287c58323a69f2a5b60a1356c7
Isaac Dennis <isaacdennissombra@gmail.com>
2023-06-27 12:44:41 -0300

    Add comment-p and entry-p functions

--- tr-mode.el
+++ tr-mode.el
@@ -4,3 +4,16 @@
 (defvar tr-mode-map (make-sparse-keymap)
   "Keymap for TR mode")
 
+(defun line-indentation-position ()
+  (save-excursion (back-to-indentation) (point)))
+
+(defun comment-p ()
+  "Returns t if the current line is a comment. Otherwise, nil"
+  (= (char-after (line-indentation-position)) ?#))
+
+(defun entry-p ()
+  "Returns t if the current line is a entry. Otherwise, nil"
+  (let* ((beg (line-beginning-position))
+		 (end (line-end-position))
+		 (content (buffer-substring-no-properties beg end)))
+	(and (not (equal (string-match-p "[^@]=" content) nil)) (not (comment-p)))))
