8a1bb33f2a7fc8172b2ab2365d8641d3caea6fb0
Isaac Dennis <isaacdennissombra@gmail.com>
2023-06-28 00:24:51 -0300

    Change predicate functions to accept line content as an argument

--- tr-mode.el
+++ tr-mode.el
@@ -10,44 +10,50 @@
 (setq tr-mode-highlights
 	  '(("^#\\(.*\\)$" . 'font-lock-comment-face)
 		(".*[^@]=" . 'font-lock-string-face)))
- 
+
+(defun get-current-line-content ()
+  "Return the characters of the current line."
+  (let ((beg (line-beginning-position))
+		(end (line-end-position)))
+	(buffer-substring-no-properties beg end)))
+
 (defun line-indentation-position ()
   (save-excursion (back-to-indentation) (point)))
 
-(defun comment-p ()
-  "Return t if the current line is a comment.  Otherwise, nil."
-  (= (char-after (line-indentation-position)) ?#))
+(defun separator-match (content)
+  "Return index of start of separator match in CONTENT."
+  (string-match-p "[^@]=" content))
 
-(defun entry-p ()
-  "Return t if the current line is a entry.  Otherwise, nil."
-  (let* ((beg (line-beginning-position))
-		 (end (line-end-position))
-		 (content (buffer-substring-no-properties beg end)))
-	(and (not (equal (string-match-p "[^@]=" content) nil)) (not (comment-p)))))
+(defun comment-p (content)
+  "Return t if CONTENT is a comment."
+  (equal (string-match-p "^#\\(.*\\)?" content) 0))
 
-(defun translated-p ()
-  "Return t if the current entry is translated.  Otherwise, nil."
-  (let* ((beg (line-beginning-position))
-		 (end (line-end-position))
-		 (content (buffer-substring-no-properties beg end))
-		 (last-index (1- (length content))))
-	(and (entry-p) (not (= (1+ (string-match-p "[^@]=" content)) last-index)))))
+(defun entry-p (content)
+  "Return non-nil (the index of start of separator match) if CONTENT is a entry."
+  (unless (comment-p content)
+	(separator-match content)))
 
-(defun next-entry ()
-  (interactive)
-  (while (and (= (forward-line) 0) (not (entry-p))))
-  (when (entry-p)
-	(re-search-forward "[^@]=")))
+(defun translated-p (content)
+  (let ((last-index (1- (length content)))
+		(sep-index (entry-p content)))
+	(unless (equal sep-index nil)
+	  (not (= (1+ sep-index) last-index)))))
 
-(defun previous-entry ()
-  (interactive)
-  (while (and (= (forward-line -1) 0) (not (entry-p))))
-  (when (entry-p)
-	(re-search-forward "[^@]=")))
+;;;(defun next-entry ()
+;;;  (interactive)
+;;;  (while (and (= (forward-line) 0) (not (entry-p))))
+;;;  (when (entry-p)
+;;;	(re-search-forward "[^@]=")))
 
-(defun next-untranslated-entry ()
-  (interactive)
-  (while (progn
-		   (next-entry)
-		   (translated-p))))
+;;;(defun previous-entry ()
+;;;  (interactive)
+;;;  (while (and (= (forward-line -1) 0) (not (entry-p))))
+;;;  (when (entry-p)
+;;;	(re-search-forward "[^@]=")))
+
+;;;(defun next-untranslated-entry ()
+;;;  (interactive)
+;;;  (while (progn
+;;;		   (next-entry)
+;;;		   (translated-p))))
 
